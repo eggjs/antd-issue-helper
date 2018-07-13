fa13a73b3f787c11756eceeb7aa84b0bd75efcd8
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2018-07-13 15:51:30 +0800

    chore: add cssModule

--- config-overrides.js
+++ config-overrides.js
@@ -1,17 +1,21 @@
+/* config-overrides.js */
+
 const path = require('path');
 const tsImportPluginFactory = require('ts-import-plugin')
-const { getLoader } = require("react-app-rewired");
+const rewireLess = require('./utils/createRewireLess');
+
+const {
+  getLoader
+} = require("react-app-rewired");
 
 module.exports = function override(config, env) {
+
   config.entry.unshift(
-    path.resolve(__dirname, './src/en-US.ts'),
-    path.resolve(__dirname, './src/zh-CN.ts'),
+    path.resolve(__dirname, './src/i18n/en-US.ts'),
+    path.resolve(__dirname, './src/i18n/zh-CN.ts'),
   );
 
-  const rewireLess = require('react-app-rewire-less-modules')
-  config = rewireLess(config, env)
-
-  config.module.rules.push({ 
+  config.module.rules.push({
     test: /\.md$/,
     use: [
       'html-loader',
@@ -22,30 +26,38 @@ module.exports = function override(config, env) {
   const fileLoader = getLoader(
     config.module.rules,
     rule =>
-      rule.loader &&
-      typeof rule.loader === 'string' &&
-      rule.loader.includes('file-loader')
+    rule.loader &&
+    typeof rule.loader === 'string' &&
+    rule.loader.includes('file-loader')
   );
 
   fileLoader.exclude.push(/\.md$/);
 
+
   const tsLoader = getLoader(
     config.module.rules,
     rule =>
-      rule.loader &&
-      typeof rule.loader === 'string' &&
-      rule.loader.includes('ts-loader')
+    rule.loader &&
+    typeof rule.loader === 'string' &&
+    rule.loader.includes('ts-loader')
   );
 
   tsLoader.options = {
     getCustomTransformers: () => ({
-      before: [ tsImportPluginFactory({
-        libraryName: 'antd',
+      before: [tsImportPluginFactory({
         libraryDirectory: 'es',
+        libraryName: 'antd',
         style: true,
-      }) ]
+      })]
     })
-  }
+  };
 
-  return config
+  config = rewireLess.withLoaderOptions({
+    javascriptEnabled: true,
+    modifyVars: {
+      "@primary-color": "#1DA57A"
+    },
+  })(config, env);
+
+  return config;
 }
